3c9660ea1f4921e5b4942f30514e49f5576213a3
EvgenyDavydenko <frontend82@mail.ru>
2022-04-08 12:52:25 +0300

    add bootstrap to view

--- views/shop/index.php
+++ views/shop/index.php
@@ -1,4 +1,20 @@
-<?php
-echo '<pre>';
-var_dump($goods);
-echo '</pre>';
+<div class="row justify-content-center">
+
+<?php foreach ($goods as $good): ?>
+
+<div class="col-4">
+  <div class="card" style="width: 16rem;">
+    <img src="/img/<?= $good['img'] ?>" class="card-img-top" alt="<?= $good['name'] ?>">
+    <div class="card-body">
+      <h5 class="card-title"><?= $good['name'] ?></h5>
+      <p class="card-text">Состав: <?= $good['composition'] ?></p>
+      <p class="card-text">Цена: <?= $good['price'] ?> рублей</p>
+      <a href="#" class="btn btn-success">Заказать</a>
+      <a href="#" class="btn btn-primary">Подробнее</a>
+    </div>
+  </div>
+</div>
+
+<?php endforeach ?>
+
+</div>
